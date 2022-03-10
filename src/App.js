1b2aa54a8622dc54eb4cf081ad15c58266160493
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2022-03-10 19:07:13 -0300

    create card component and fetch api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import { Card } from './components/Card';
+import React, { useState, useEffect } from 'react'
+import { Card } from './components/Card'
 
+const App = () => {
+  const [character, setCharacter] = useState([{}])
 
-function App() {
+  useEffect(() => {
+    getData()    
+  }, [])
+
+  async function getData() {
+    await fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((results) => setCharacter(results.results))          
+  }
+  
+
   return (
-    <Card />
-  );
+    <>
+      <h1>Rick and Morty</h1>      
+      <div>
+          {character.map(item => <Card id={item.id} name={item.name} status={item.status} species={item.species} gender={item.gender} />)}        
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
