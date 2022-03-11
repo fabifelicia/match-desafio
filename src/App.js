8de5eca1b54f1b019d4316dd674ccd234f74a98b
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2022-03-10 22:08:41 -0300

    styling cards with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, { useState, useEffect } from 'react'
-import { Card } from './components/Card'
+import Header from './components/Header'
+import { Cards } from './components/Cards'
+import Container from 'react-bootstrap/Container'
 
 const App = () => {
   const [character, setCharacter] = useState([{}])
 
   useEffect(() => {
-    getData()    
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((data) => setCharacter(data.results))
   }, [])
 
-  async function getData() {
-    await fetch('https://rickandmortyapi.com/api/character')
-      .then((response) => response.json())
-      .then((results) => setCharacter(results.results))          
-  }
-  
-
   return (
-    <>
-      <h1>Rick and Morty</h1>      
-      <div>
-          {character.map(item => <Card id={item.id} name={item.name} status={item.status} species={item.species} gender={item.gender} />)}        
+    <Container className='mt-3'>
+      <Header />
+      <div
+        className='mt-3'
+        style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)' }}
+      >
+        {character.map((item) => (
+          <Cards
+            id={item.id}
+            name={item.name}
+            status={item.status}
+            species={item.species}
+            gender={item.gender}
+          />
+        ))}
       </div>
-    </>
+    </Container>
   )
 }
 
