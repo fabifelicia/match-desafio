939e139e51503bcdfee3344d1a3cfc7cf2190d68
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2022-03-13 20:20:14 -0300

    styling card component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,14 +3,21 @@ import Card from 'react-bootstrap/Card'
 
 export function Cards({ id, name, species }) {
   return (
-    <Card className='mb-3 shadow' style={{ width: '12rem'}}>
+    <Card className='mb-3 shadow' style={{ width: '12rem' }}>
       <Card.Img
         variant='top'
         src={`https://rickandmortyapi.com/api/character/avatar/${id}.jpeg`}
       />
-      <Card.Body>
-        <Card.Title style={{fontFamily: 'Roboto'}} className='text-center fs-5 fw-bold'>{name}</Card.Title>
-        <Card.Text style={{fontFamily: 'Roboto'}} className='text-center'>{species}</Card.Text>        
+      <Card.Body style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between'}}>
+        <Card.Title
+          style={{ fontFamily: 'Roboto' }}
+          className='text-center fs-5 fw-bold'
+        >
+          {name}
+        </Card.Title>
+        <Card.Text style={{ fontFamily: 'Roboto' }} className='text-center'>
+          {species}
+        </Card.Text>
       </Card.Body>
     </Card>
   )
